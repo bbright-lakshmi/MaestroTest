a33b43135e674c90f9bb2448623dc0ca4be9d3ea
lakshmi <lakshmi.addepalli@billionbright.org>
2024-09-17 11:42:41 +0530

    Grofast

--- myscript.js
+++
@@ -1,6 +0,0 @@
-var deviceDetails = { platform: device.platform, model: device.model, 
-    manufacturer: device.manufacturer, osVersion: device.version,
-      }; 
-    var deviceDetailsString = JSON.stringify(deviceDetails); output.myFlow = deviceDetailsString;
-    
-   
